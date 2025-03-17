907caa3a753c60bbfde6c0c478b0ec3e26e5b36d
Adec09 <adrian.ecija.9@gmail.com>
2025-03-17 06:09:28 +0100

    Sistema de apuntado

--- Assets/Scripts/AimStates/AimStateManager.cs
+++ Assets/Scripts/AimStates/AimStateManager.cs
@@ -6,13 +6,20 @@ using Unity.Cinemachine;
 public class AimStateManager : MonoBehaviour
 {
 
+    AimBaseState currentState;
+    public HipFireState Hip = new HipFireState();
+    public AimState Aim = new AimState();
+
     [SerializeField] float mouseSense = 1;
     float xAxis, yAxis;
     [SerializeField] Transform cameraFollowPosition;
+
+    [HideInInspector] public Animator anim;
     
     void Start()
     {
-        
+        anim = GetComponentInChildren<Animator>();
+        SwitchState(Hip);
     }
 
     
@@ -21,11 +28,19 @@ public class AimStateManager : MonoBehaviour
         xAxis += Input.GetAxisRaw("Mouse X") * mouseSense;
         yAxis -= Input.GetAxisRaw("Mouse Y") * mouseSense;
         yAxis = Mathf.Clamp(yAxis, -80, 80);
+
+        currentState.UpdateState(this);
     }
 
     private void LateUpdate()
     {
         cameraFollowPosition.localEulerAngles = new Vector3(yAxis, cameraFollowPosition.localEulerAngles.y, cameraFollowPosition.localEulerAngles.z);
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis, transform.eulerAngles.z);
+    }
+
+    public void SwitchState(AimBaseState state)
+    {
+        currentState = state;
+        currentState.EnterState(this);
     }
 }
