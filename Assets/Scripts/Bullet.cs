02516a19d2561296077c346a793bf7a345b8b49c
Adec09 <adrian.ecija.9@gmail.com>
2025-03-29 12:22:48 +0100

    Enemy & Enemy health script

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,21 +3,25 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] float timeToDestroy;
-    float timer;
+    [HideInInspector] public WeaponManager weapon;
+    
     void Start()
     {
-        
+        Destroy(this.gameObject, timeToDestroy);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        timer += Time.deltaTime;
-        if(timer >= timeToDestroy) Destroy(this.gameObject);
-    }
+   
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.GetComponentInParent<EnemyHealth>())
+        {
+            EnemyHealth enemyHealth = collision.gameObject.GetComponentInParent<EnemyHealth>();
+            enemyHealth.TakeDamage(weapon.damage);
+        }
+
         Destroy(this.gameObject);
+
+
     }
 }
