9eb26952051ec75783d2d45bb0e66f0a19927299
Adec09 <adrian.ecija.9@gmail.com>
2025-03-30 07:34:44 +0200

    Added Main Menu scene & funcional buttons

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigation : MonoBehaviour
+{
+    public Object sceneToLoad;
+
+    public void LoadSelectedScene()
+    {
+        if (sceneToLoad != null)
+        {
+            string sceneName = sceneToLoad.name;
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.Log("No se ha asignado una escena en el Inspector.");
+        }
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+    }
+}
+
